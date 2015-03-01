83379a9dd89d1ad0dc672eb2961b8298e11bad1f
Pradeep Susarla <pradeep@pradeeps.org>
2015-03-01 23:45:35 +0000

    Add pins/users association

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user! , except: [:index, :show]
+  before_action :correct_user , only: [:edit, :udpate, :destroy]
 
   # GET /pins
   # GET /pins.json
@@ -68,6 +69,12 @@ class PinsController < ApplicationController
     def set_pin
       @pin = Pin.find(params[:id])
     end
+
+    def correct_user
+    @pin = current_user.pins.find_by(id: params[:id])
+    redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+    end
+
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
