48fbffde0429fb547f788ad65c726973e2e717d5
Pradeep Susarla <pradeep@pradeeps.org>
2015-05-10 22:52:14 +0100

    added FB secret and appID

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, 'YOUR-APP-ID-HERE', 'YOUR-APP-SECRET-HERE'
+  provider :facebook, '657912274338925', 'a42cb2ec24c65a4ed3b1ec6ec8a09c8e'
 end
